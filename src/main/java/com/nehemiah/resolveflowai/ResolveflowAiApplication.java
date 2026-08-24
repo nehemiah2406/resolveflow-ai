591ddfd9eb0aa09236b95c536bba2cd228c69e15
nehemiah2406 <nehemiahdot@gmail.com>
2026-08-24 20:44:25 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/nehemiah/resolveflowai/ResolveflowAiApplication.java
@@ -0,0 +1,13 @@
+package com.nehemiah.resolveflowai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveflowAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolveflowAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nehemiah/resolveflowai/ResolveflowAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nehemiah.resolveflowai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolveflowAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
